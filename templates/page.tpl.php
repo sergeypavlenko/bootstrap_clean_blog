5f55a2bbd8c136915ecf94e5186e8bb2ee9f1082
Sergey Pavlenko <sergeypavlenko@1343804.no-reply.drupal.org>
2015-05-10 23:17:57 +0300

    Fixed social network links.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -161,7 +161,7 @@
         <ul class="list-inline text-center">
           <li>
             <a target="_blank"
-               href="<?php print theme_get_setting('social_facebook'); ?>">
+               href="<?php print theme_get_setting('social_twitter'); ?>">
               <span class="fa-stack fa-lg">
                 <i class="fa fa-circle fa-stack-2x"></i>
                 <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
@@ -170,7 +170,7 @@
           </li>
           <li>
             <a target="_blank"
-               href="<?php print theme_get_setting('social_twitter'); ?>">
+               href="<?php print theme_get_setting('social_facebook'); ?>">
               <span class="fa-stack fa-lg">
                 <i class="fa fa-circle fa-stack-2x"></i>
                 <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
